3ea544ae03562d80db6716ee45176ef89de32cf0
Danielacoco <danielacoor6@gmail.com>
2017-04-02 18:58:28 -0700

    Chain construction and bone orientation

---
+++ Assets/CylinderOrientation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderOrientation : MonoBehaviour {
+
+	public Transform lookAtPoint;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 newRotation = (lookAtPoint.position - transform.position);
+		//Quaternion toRot = Quaternion.FromToRotation (transform.up, newRotation);
+		transform.rotation = Quaternion.FromToRotation (transform.up, newRotation);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newRotation = (lookAtPoint.position - transform.position);
+		//Quaternion toRot = Quaternion.FromToRotation (transform.up, newRotation);
+		transform.rotation = Quaternion.FromToRotation (transform.up, newRotation);
+	}
+}
