ffa60921914ef01a8cac47d8fb2903ebd8fc9e7a
Danielacoco <danielacoor6@gmail.com>
2017-04-03 15:55:31 -0700

    chain construction and bone orientation done, base case for FABRIK algorithm

---
+++ Assets/Scripts/CylinderOrientation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderOrientation : MonoBehaviour {
+
+	public Transform lookAtPoint;
+	public Transform startPoint;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		SetPosition (startPoint.position, lookAtPoint.position);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetPosition (startPoint.position, lookAtPoint.position);
+//		Vector3 newRotation = (lookAtPoint.position - transform.position);
+//		//Quaternion toRot = Quaternion.FromToRotation (transform.up, newRotation);
+//		transform.rotation = Quaternion.Lerp(transform.up, newRotation, );
+	}
+
+	void SetPosition(Vector3 start, Vector3 end){
+		var direction = end - start;
+		var midd = (direction) * 0.5f + start;
+		transform.position = midd;
+		transform.rotation = Quaternion.FromToRotation (Vector3.up, direction);
+	}
+}
---
+++ Assets/Scripts/FABRIKSingleChain.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FABRIKSingleChain : MonoBehaviour {
+
+	public Transform[] joints;
+
+
+	public float tolerance = 0.1f;
+
+	public Transform origin;
+	public float[] distances;
+
+
+	private float totalLength = 0;
+
+	private float distToTarget;
+
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+
+		foreach (float l in distances) {
+			totalLength += l;
+		}
+
+		SolveSingleChainFABRIK ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SolveSingleChainFABRIK ();
+		
+	}
+
+	void SolveSingleChainFABRIK(){
+		distToTarget = (Target.position - origin.position).magnitude;
+
+		if (distToTarget > totalLength) {
+			//target too far, out of reach
+			for (int i = 0; i < joints.Length - 1; i++) {
+				float r = (Target.position - joints [i].position).magnitude;
+				float lambda = (distances [i] / r);
+				joints [i + 1].position = (1 - lambda) * joints [i].position + lambda * Target.position;
+			}
+		} else {
+			// target is reachable, we have our origin joint saved
+			// check whether the distance btw end effector and target is grater than our tolerance 
+			float endToTargetDist =  (Target.position - joints[joints.Length-1].position).magnitude;
+			while (endToTargetDist > tolerance) {
+				//do forward
+				//do backward
+			}
+
+
+		}
+	
+	}
+}
