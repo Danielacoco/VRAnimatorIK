fc2e28ee92b82c53f8a99daf67d327b1ba5a1e10
Danielacoco <danielacoor6@gmail.com>
2017-04-05 18:56:18 -0700

    fix Target

--- Assets/Scripts/FABRIKChain.cs
+++ Assets/Scripts/FABRIKChain.cs
@@ -2,57 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FABRIKSingleChain : MonoBehaviour {
+public class FABRIKChain : MonoBehaviour {
 
+	// array of joints and distance, we could generate distances on the fly, but for now they are determined by the user
 	public Transform[] joints;
+	public float[] distances;
 
+	public int numJoints;
 
 	public float tolerance = 0.1f;
 
+	// base position
 	public Vector3 origin;
-	public float[] distances;
+	// target obj
+	public Vector3 Target;
+
 
 
 	private float totalLength = 0;
 
 	private float distToTarget;
 
-	public Transform Target;
+
 
 	// Use this for initialization
 	void Start () {
 
+
+
 		foreach (float l in distances) {
 			totalLength += l;
 		}
+		numJoints = joints.Length;
+		Debug.Log ("numJoints");
+		Debug.Log (numJoints);
+		Debug.Log ("numJoints");
 
-		SolveSingleChainFABRIK ();
+
+		//SolveChainFABRIK ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		SolveSingleChainFABRIK ();
+		//SolveChainFABRIK ();
 		
 	}
 
-	void SolveSingleChainFABRIK(){
-		distToTarget = (Target.position - origin).magnitude;
+	public void SolveChainFABRIK(){
+		distToTarget = (Target - origin).magnitude;
 
 		if (distToTarget > totalLength) {
 			//target too far, out of reach
-			for (int i = 0; i < joints.Length - 1; i++) {
-				float r = (Target.position - joints [i].position).magnitude;
+			for (int i = 0; i < numJoints - 1; i++) {
+				float r = (Target - joints [i].position).magnitude;
 				float lambda = (distances [i] / r);
-				joints [i + 1].position = (1 - lambda) * joints [i].position + lambda * Target.position;
+				joints [i + 1].position = (1 - lambda) * joints [i].position + lambda * Target;
 			}
 		} else {
 			// target is reachable, we have our origin joint saved
 			// check whether the distance btw end effector and target is grater than our tolerance 
-			float endToTargetDist =  (Target.position - joints[joints.Length-1].position).magnitude;
+			float endToTargetDist =  (Target - joints[numJoints-1].position).magnitude;
 			while (endToTargetDist > tolerance) {
 				ForwardStep ();
 				BackwardStep ();
-				endToTargetDist = (Target.position - joints[joints.Length-1].position).magnitude;
+				endToTargetDist = (Target - joints[numJoints-1].position).magnitude;
 			}
 
 
@@ -60,9 +73,10 @@ public class FABRIKSingleChain : MonoBehaviour {
 	
 	}
 
-	void ForwardStep(){
-		joints [joints.Length - 1].position = Target.position;
-		for (int i = joints.Length - 2; i >= 0; i--) {
+	public void ForwardStep(){
+		Debug.Log (numJoints - 1);
+		joints [numJoints - 1].position = Target;
+		for (int i = numJoints - 2; i >= 0; i--) {
 			//Debug.Log (i);
 			float dist = (joints[i + 1].position - joints [i].position).magnitude;
 			float lambda = distances [i] / dist;
@@ -71,11 +85,11 @@ public class FABRIKSingleChain : MonoBehaviour {
 		}
 	}
 
-	void BackwardStep(){
+	public void BackwardStep(){
 		//set first joint to origin
 		joints [0].position = origin;
 
-		for (int i = 0; i < joints.Length - 2; i++) {
+		for (int i = 0; i < numJoints - 2; i++) {
 			float dist = (joints [i + 1].position - joints [i].position).magnitude;
 			float lambda = (distances [i] / dist);
 			// new joint position
---
+++ Assets/Scripts/FABRIKMulti.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FABRIKMulti : MonoBehaviour {
+	
+	public FABRIKChain[] chains;
+	int numChains;
+	// Use this for initialization
+
+	void Awake(){
+		numChains = chains.Length;
+		Debug.Log (numChains);
+	}
+	void Start () {
+		//SolveFABRIKMulti ();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SolveFABRIKMulti ();
+		
+	}
+
+	void SolveFABRIKMulti(){
+		//set origin 
+		//chains [1].origin = chains[1].joints[0].position;
+		Debug.Log ("after 1");
+		//chains [2].origin = chains[2].joints[0].position;
+		Debug.Log ("after 2");
+		chains [1].BackwardStep ();
+		Debug.Log ("backwardchain1 3");
+
+		Vector3 posChain1 = (chains [1].joints [0].position );
+
+		chains [2].BackwardStep ();
+		Debug.Log ("backwardchain2 3");
+
+		Vector3 posChain2 = (chains[2].joints[0].position);
+
+		Vector3 centroid = (posChain1 + posChain2) / 2;
+
+//		chains [1].origin = centroid;
+//		chains [1].joints [0].position = centroid;
+//		chains [2].joints [0].position = centroid;
+
+			
+		//chains [1].joints [0].position = 0;
+		chains [0].Target= centroid; //-------->bug here?
+		Debug.Log ("after updating target");
+		chains [0].SolveChainFABRIK ();
+		//chains[0].BackwardStep()
+		chains [1].origin = chains [0].joints[chains[0].numJoints-1].position;
+		chains [2].origin = chains [0].joints[chains[0].numJoints-1].position;
+		Debug.Log ("solving finalchain backwards and forwards");
+		chains [1].ForwardStep ();
+		Debug.Log ("forward1");
+		chains [2].ForwardStep ();
+		Debug.Log ("forward 2");
+
+
+		
+	}
+}
